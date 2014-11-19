f94ff74b2cae5476ec378b0ea9d48ec6476e81cf
Łukasz Pająk <l.pajak@smsapi.pl>
2014-11-19 02:06:59 +0100

    add BuiltinsTest::testMockExceptionClass (marked as skipped for now, but should be working in the future)

--- tests/Phockito/BuiltinsTest.php
+++ tests/Phockito/BuiltinsTest.php
@@ -3,6 +3,10 @@
 namespace Phockito;
 
 
+use Exception;
+use Hamcrest\Core\IsInstanceOf;
+use Hamcrest\MatcherAssert;
+use Hamcrest\Type\IsString;
 use PHPUnit_Framework_TestCase;
 use SoapClient;
 
@@ -15,5 +19,16 @@ class BuiltinsTest extends PHPUnit_Framework_TestCase
         $this->assertInstanceOf(SoapClient::class, $mock);
         $this->assertNull($mock->Foo());
         $this->assertNull($mock->Bar());
+    }
+
+    public function testMockExceptionClass()
+    {
+        $this->markTestSkipped('class contains final methods');
+
+        $mock = Phockito::mock(Exception::class);
+
+        MatcherAssert::assertThat($mock, new IsInstanceOf(Exception::class));
+        MatcherAssert::assertThat($mock->getMessage(), new IsString());
+        MatcherAssert::assertThat($mock->getCode(), new IsString());
     }
 }
