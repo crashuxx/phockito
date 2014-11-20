eb2e45b701fdcc6943e0d4fdaf6eb509029cc3de
Łukasz Pająk <l.pajak@smsapi.pl>
2014-11-20 01:52:28 +0100

    suppress pmd warning

--- tests/Phockito/Test/SpyMe.php
+++ tests/Phockito/Test/SpyMe.php
@@ -9,6 +9,10 @@ class SpyMe {
     public $constructor_arg = false;
     function __construct($arg = true) { $this->constructor_arg = $arg; }
 
+    /**
+     * @throws Exception
+     * @return null
+     */
     function Foo() { throw new Exception('Base method Foo was called'); }
     function Bar() { return $this->Foo(); }
     function Baz($response) { return $response; }
